549554275ed17796978ff25d2fbd5313b0b3c17a
four-leaf-4 <kojima.koike.lab@gmai>
2020-12-23 17:42:56 +0900

    add lib

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData, getDummyPostData } from "../lib/posts";
 
-export default function Home() {
+export default function Home({ allPostsData, dummyPostData }) {
+  console.log(dummyPostData);
+  console.log(allPostsData);
   return (
     <Layout home>
       <Head>
@@ -19,6 +22,32 @@ export default function Home() {
           <a>FIRST POST</a>
         </Link>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  const dummyPostData = await getDummyPostData();
+  return {
+    props: {
+      allPostsData,
+      dummyPostData,
+    },
+  };
 }
